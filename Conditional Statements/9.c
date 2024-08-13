97f773f7428f7719ea6198a5bfab23485122dc79
bhumijain26-4 <jainbhumi1331@gmail.com>
2024-08-13 23:38:18 +0530

    Update 9.c

--- Conditional Statements/9.c
+++ Conditional Statements/9.c
@@ -3,17 +3,24 @@
 #include<string.h>
 int main()
 {
-	char name[100];
-	printf("\nEnter your name: ");
-	gets(name);
-	if(name==strupr(name))
-	{
-		printf("\nIt is uppercase");
-	}
-	else if(name==strlwr(name))
-	{
-		printf("\nIt is lowercase");
-	}
-	
-	return 0;
+   char ch;
+   printf("\nEnter a character: ");
+   scanf(" %c",&ch);
+   if(ch>='A' && ch<='Z')
+   {
+   	printf("\n%c is an Uppercase character",ch);
+   }
+    else if(ch>='a' && ch<='z')
+   {
+   	printf("\n%c is Lowercase character",ch);
+   }
+    else if(ch>='0' && ch<='9')
+   {
+   	printf("\n%c is a Digit",ch);
+   }
+    else
+   {
+   	printf("\n%c is a Special Character",ch);
+   }
+   return 0;
 }
